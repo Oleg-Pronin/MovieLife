e6a967a986de1acd0254618402bfd82bb151f0c2
Oleg Pronin <o.pronin@qsoft.ru>
2021-11-05 19:09:15 +0300

    Init commit

---
+++ app/src/main/java/oleg_pronin/movielife/ui/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package oleg_pronin.movielife.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import oleg_pronin.movielife.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
