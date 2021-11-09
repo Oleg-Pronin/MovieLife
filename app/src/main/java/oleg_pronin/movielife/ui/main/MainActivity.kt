984e66602ee3a4af65caddb30e192b8de2880bf9
Oleg Pronin <o.pronin@qsoft.ru>
2021-11-09 23:07:39 +0300

    Создание фрагментов приложения, добавление нижней навигации и настройка взаимодействия меню и фрагментов через компонент навигации

--- app/src/main/java/oleg_pronin/movielife/ui/main/MainActivity.kt
+++ app/src/main/java/oleg_pronin/movielife/ui/main/MainActivity.kt
@@ -2,12 +2,44 @@ package oleg_pronin.movielife.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import oleg_pronin.movielife.R
+import oleg_pronin.movielife.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        initNavigation()
     }
 
+    private fun getNavController(): NavController {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        return navHostFragment.navController
+    }
+
+    private fun initNavigation() {
+        val navController = getNavController()
+
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        binding.bottomNavigation.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return getNavController().navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
 }
